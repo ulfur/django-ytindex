729330fa7b421fcc81c0660bd4fc65fa24bd466d
Úlfur Kristjánsson <ulfurk@ulfurk-dev.local>
2019-05-16 00:46:31 +0200

    Fixing url config

--- ytindex/urls.py
+++ ytindex/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from .views import search
 
 urlpatterns = [
-    re_path('(?P<index>\w*)/(?P<query>.*)', search, name='ytindex-search'),
+    re_path('/(?P<index>\w*)/(?P<query>.*)', search, name='ytindex-search'),
 ]
